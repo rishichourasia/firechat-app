130303a1c74ef71880769ed23c43d933a8cea0ba
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-10-04 23:51:39 +0530

    context file done

--- src/components/Context.js
+++ src/components/Context.js
@@ -5,3 +5,25 @@ import { auth } from "../firebase";
 const authContext = React.createContext(auth);
 
 export const useAuth = () => createContext(authContext);
+
+export const AuthProvider = ({ children }) => {
+	const [loading, setloading] = useState(true);
+	const [user, setUser] = useState({});
+	const history = usehistory();
+
+	useEffect(() => {
+		auth.onAuthStateChanged((user) => {
+			setUser(user);
+			setloading(false);
+			history.push("/chats");
+		});
+	}, [user, history]);
+
+	const value = { user };
+
+	return (
+		<AuthProvider.Provider value={value}>
+			{!loading && children}
+		</AuthProvider.Provider>
+	);
+};
