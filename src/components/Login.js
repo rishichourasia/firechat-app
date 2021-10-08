2ac95516e9ac5948fda96ba6b1ef7e35d3d0de34
Rishi Chourasia <rishichourasia2100@gmail.com>
2021-10-08 20:42:47 +0530

    login page css

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,16 +6,24 @@ import { auth } from "../firebase";
 
 export const Login = () => {
 	return (
-		<div className="login-page">
-			<div className="login-box">
-				<div
-					className="btn"
-					onClick={() =>
-						auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
-					}
-				>
-					<GoogleOutlined /> Sign in
+		<div className="over">
+			<div classname="card text-center  border-dark">
+				<div className="card-header text-muted">Login/Signup</div>
+				<div className="card-body">
+					<h3 className="card-title">Firechat</h3>
+					<p className="card-text">
+						Sign In Using Google and start using Firechat rightaway !
+					</p>
+					<div
+						className="btn btn-primary"
+						onClick={() =>
+							auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+						}
+					>
+						<GoogleOutlined /> Login
+					</div>
 				</div>
+				<div class="card-footer text-muted">© Rishi Chourasia 2021</div>
 			</div>
 		</div>
 	);
